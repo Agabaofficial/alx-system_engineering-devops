b5ca31739c2a542d5be6015e57e8f8ebdda3bffc
Agabaofficial <agabaolivier85@gmail.com>
2023-11-07 17:38:32 +0200

    .py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" module for function to return top 10 hot posts of a given subreddit """
+"""this function is used to return top 10 hot posts of a given subreddit """
 import requests
 import sys
 after = None
