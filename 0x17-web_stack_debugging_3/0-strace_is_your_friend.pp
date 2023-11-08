f27c9a213ff7cd3fa328dad542297bdf2f4d1001
Agabaofficial <agabaolivier85@gmail.com>
2023-11-08 16:18:51 +0200

    exec

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,5 @@
 # task 0 _ automated puppet fix (to find out why Apache is returning a 500 error)
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-    provider => shell,
+exec { 'fixed-phpp':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => '/bin';
     }
